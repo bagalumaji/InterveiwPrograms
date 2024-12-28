1415fed58bae45765c0e552ea305347ba23037b2
umaji bagal <bagalumaji@gmail.com>
2024-12-28 14:57:52 +0530

    RemoveDuplicates

---
+++ src/arrays/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int nums[] = {1,1,2,2,3,4,4,5,5};
+        int cnt = removeDuplicates(nums);
+        for (int i = 0; i <cnt; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+    static int removeDuplicates(int[] nums) {
+        int start = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i-1]){
+                nums[++start] = nums[i];
+            }
+        }
+        return ++start;
+    }
+}
